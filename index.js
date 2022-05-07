e319af57236ef13e63bb1d1f7cadef9c42deedfe
kanakweb <kanakwebdev@gmail.com>
2022-05-07 15:49:58 +0600

    some of jwt done

--- index.js
+++ index.js
@@ -15,25 +15,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 //Verify
-function verifyJWT(req,res,next){
-  const authHeader=req.headers.authorization
-  if(!authHeader){
-    return res.status(401).send({message:'unAuthorized access'})
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unAuthorized access" });
   }
-  const token=authHeader.split(' ')[1];
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-    if(err){
-      return res.status(403).send({message:'Forbidden access'})
+  const token = authHeader.split(" ")[1];
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    (err, decoded) => {
+      if (err) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      console.log("decoded", decoded);
+      req.decoded = decoded;
+      next();
     }
-    console.log('decoded',decoded);
-    req.decoded=decoded
-  })
-  
-next()
+  );
 }
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfzcd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -140,17 +141,16 @@ async function run() {
     });
 
     //add user items..
-    app.get("/inventoryItems",verifyJWT, async (req, res) => {
-      const decodedEmail=req.decoded.email
+    app.get("/inventoryItems", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      if(email===decodedEmail){
-        const query = {email:email};
-      const cursor = inventoryItemsCollection.find(query);
-      const addUserItems = await cursor.toArray();
-      res.send(addUserItems);
-      }
-      else{
-        res.status(403).send({message:'forbidden access'})
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = inventoryItemsCollection.find(query);
+        const addUserItems = await cursor.toArray();
+        res.send(addUserItems);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
       }
     });
   } finally {
